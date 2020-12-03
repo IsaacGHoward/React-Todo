492bb53b66a6e0857a03273a0fc1d2c4d4d7ce1a
IsaacGHoward <howardisaac100@gmail.com>
2020-12-02 17:22:11 -0800

    Set up basic todolist and todo components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,25 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from 'react';
+import Todo from './Todo';
+export default class TodoList extends Component{
+    constructor() {
+        super();
+        this.mapTodos = this.mapTodos.bind(this);
+    }
+    mapTodos = () => {
+        this.props.todos.map((todo) => (
+            <p key={todo.id}>{todo.task}</p>
+        ))
+    }
+    render(){
+        return (
+          //<h1>TodoList: {this.props.todos}</h1>
+          <div>
+            {
+              this.mapTodos()
+            }
+          </div>
+        );
+    }
+}

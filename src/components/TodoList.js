02984641858d7fe15788df542c49ba4949858943
IsaacGHoward <howardisaac100@gmail.com>
2020-12-02 17:25:38 -0800

    TodoList shows todos from App

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,13 +8,14 @@ export default class TodoList extends Component{
         this.mapTodos = this.mapTodos.bind(this);
     }
     mapTodos = () => {
-        this.props.todos.map((todo) => (
-            <p key={todo.id}>{todo.task}</p>
-        ))
+        return(
+            this.props.todos.map((todo) => (
+                <Todo task={todo.task} id={todo.id} completed={todo.completed}/>
+            ))
+        )
     }
     render(){
         return (
-          //<h1>TodoList: {this.props.todos}</h1>
           <div>
             {
               this.mapTodos()
